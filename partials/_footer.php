9276280eae6f3b99bdbe513fc7a19ebb916ae8c6
eddyricchy <eddyricchy@gmail.com>
2022-09-09 00:39:34 +0100

    fixed indentation in _footer.php file

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,13 +1,13 @@
 <footer>
-				<div class="social-media">
-					<div>
-						<a href="https://www.instagram.com/thestarkcoder/" id="ig"><i class="fa-brands fa-instagram"></i></a>
-						<a href="https://www.linkedin.com/in/righteous-edewor-3b31b522a/"><i class="fa-brands fa-linkedin-in"></i></a>
-						<a href="#"><i class="fa-brands fa-twitter"></i></a>
-						<a href="https://github.com/EddyRicchy"><i class="fa-brands fa-github"></i></a>
-					</div>
-				</div>
-				<div class="copyright">
-					<span>&copy; 2022 All Rights Reserves. TheStarkCoder</span>
-				</div>
-			</footer>
+	<div class="social-media">
+		<div>
+			<a href="https://www.instagram.com/thestarkcoder/" id="ig"><i class="fa-brands fa-instagram"></i></a>
+			<a href="https://www.linkedin.com/in/righteous-edewor-3b31b522a/"><i class="fa-brands fa-linkedin-in"></i></a>
+			<a href="#"><i class="fa-brands fa-twitter"></i></a>
+			<a href="https://github.com/EddyRicchy"><i class="fa-brands fa-github"></i></a>
+		</div>
+	</div>
+	<div class="copyright">
+		<span>&copy; 2022 All Rights Reserves. TheStarkCoder</span>
+	</div>
+</footer>
