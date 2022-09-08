8dcc700f19225554e6b051a000a9da848825d983
eddyricchy <eddyricchy@gmail.com>
2022-09-08 23:08:45 +0100

    added composer.json file, procfile and .env for deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'app/thestarkcoder',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ef7cf0de2ba2e35dd6d174dc55eb08eea78a6864',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'app/thestarkcoder' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ef7cf0de2ba2e35dd6d174dc55eb08eea78a6864',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
